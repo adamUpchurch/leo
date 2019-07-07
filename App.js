a81dbd9b4bee452ade8017b6eb25e348df9bd816
Adam Upchurch <aupchurch167@gmail.com>
2019-07-07 03:16:33 -0400

    Dynamically render books in Library - need to pull from DB tho

--- App.js
+++ App.js
@@ -8,22 +8,22 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button, Alert} from 'react-native';
-import {createStackNavigator} from 'react-navigation';
-import Reading from './screens/reading';
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  createAppContainer,
+} from 'react-navigation';import Reading from './screens/reading';
 import Library from './screens/library';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+  
   render() {
     return (
       <AppStackNavigator />
     );
   }
 };
+
 
 const AppStackNavigator = createStackNavigator({
   Library: {screen: Library},
