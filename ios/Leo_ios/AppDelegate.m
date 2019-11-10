e6c5949fd12661b2c0fcbf500f7925a3241eda56
adam@ausolution.com <adam@adams-mbp.attlocal.net>
2019-11-10 16:07:10 -0500

    blah blah

--- ios/Leo_ios/AppDelegate.m
+++ ios/Leo_ios/AppDelegate.m
@@ -12,14 +12,11 @@
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
 
-@import Firebase;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  if ([FIRApp defaultApp] == nil) {
-    [FIRApp configure];
-  }
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Leo_ios"
