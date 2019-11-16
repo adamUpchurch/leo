e7f13f0432718db0c1b9ed64244ad4ddc96d8d93
adam@ausolution.com <adam@adams-mbp.attlocal.net>
2019-11-15 19:10:26 -0500

    clean up

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
-import { createStackNavigator, createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
 
 import Reading from './screens/reading';
 import Library from './screens/library';
-import AuthLoading from './screens/authLoading';
-import Login from './screens/login';
-import Signup from './screens/signup';
-import ProfileScreen from './screens/ProfileScreen';
-import HomeScreen from './screens/HomeScreen';
-
-
-
-// const AuthStack = createStackNavigator({
-//   Login: {screen: Login},
-//   Signup: {screen: Signup}
-// })
 
 const AppStack = createStackNavigator({
   Library: {screen: Library},
@@ -24,21 +12,9 @@ const AppStack = createStackNavigator({
   }
 )
 
-const Navigation = createStackNavigator({
-  Home: { screen: HomeScreen },
-  Profile: { screen: ProfileScreen }
-},
-{
-  initialRouteName: 'Home',
-}
-);
-
 export default createSwitchNavigator(
   {
-    AuthLoading: AuthLoading,
     App: AppStack,
-    // Auth: AuthStack,
-    Nav: Navigation
   },
   {
     initialRouteName: 'App',
